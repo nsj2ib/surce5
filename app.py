f727252d557f7119555b9499f79ef528e38f6716
nsj2ib <101592392+nsj2ib@users.noreply.github.com>
2023-04-03 17:15:41 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Welcome To SHARK Source'
+
+# تحياتي عبودي سكيبه
+if __name__ == "__main__":
+    app.run()
