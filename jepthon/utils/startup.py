faf3f92de2b422bb348750b795a7fa19ff47764e
nsj2ib <101592392+nsj2ib@users.noreply.github.com>
2023-04-03 17:13:02 +0300

    Add files via upload

---
+++ jepthon/utils/startup.py
@@ -0,0 +1,353 @@
+import time
+import asyncio
+import glob
+import os
+import sys
+import urllib.request
+from datetime import timedelta
+from pathlib import Path
+import requests
+from telethon import Button, functions, types, utils
+from telethon.sync import TelegramClient
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.errors.rpcerrorlist import FloodWaitError
+from jepthon import BOTLOG, BOTLOG_CHATID, PM_LOGGER_GROUP_ID
+from ..Config import Config
+from aiohttp import web
+from ..core import web_server
+from ..core.logger import logging
+from ..core.session import jepiq
+from ..helpers.utils import install_pip
+from ..helpers.utils.utils import runcmd
+from ..sql_helper.global_collection import (
+    del_keyword_collectionlist,
+    get_item_collectionlist,
+)
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from .pluginmanager import load_module
+from .tools import create_supergroup
+LOGS = logging.getLogger("jepthon")
+
+cmdhr = Config.COMMAND_HAND_LER
+bot = jepiq
+ENV = bool(os.environ.get("ENV", False))
+
+if ENV:
+    VPS_NOLOAD = ["سيرفر"]
+elif os.path.exists("config.py"):
+    VPS_NOLOAD = ["هيروكو"]
+
+async def setup_bot():
+    """
+    To set up bot for SHARK
+    """
+    try:
+        await jepiq.connect()
+        config = await jepiq(functions.help.GetConfigRequest())
+        for option in config.dc_options:
+            if option.ip_address == jepiq.session.server_address:
+                if jepiq.session.dc_id != option.id:
+                    LOGS.warning(
+                        f"⌯︙معرف ثابت في الجلسة من {jepiq.session.dc_id}"
+                        f"⌯︙لـ  {option.id}"
+                    )
+                jepiq.session.set_dc(option.id, option.ip_address, option.port)
+                jepiq.session.save()
+                break
+        bot_details = await jepiq.tgbot.get_me()
+        Config.TG_BOT_USERNAME = f"@{bot_details.username}"
+        #await jepiq.start(bot_token=Config.TG_BOT_USERNAME)
+        #app = web.AppRunner(await web_server())
+        #await app.setup()
+        #bind_address = "0.0.0.0"
+        #redaport = Config.PORT
+        #await web.TCPSite(app, bind_address, redaport).start()
+        jepiq.me = await jepiq.get_me()
+        jepiq.uid = jepiq.tgbot.uid = utils.get_peer_id(jepiq.me)
+        if Config.OWNER_ID == 0:
+            Config.OWNER_ID = utils.get_peer_id(jepiq.me)
+    except Exception as e:
+        LOGS.error(f"كـود تيرمكس - {str(e)}")
+        sys.exit()
+
+
+async def startupmessage():
+    """
+    Start up message in telegram logger group
+    """
+    try:
+        if BOTLOG:
+            Config.CATUBLOGO = await jepiq.tgbot.send_file(
+                BOTLOG_CHATID,
+                "https://telegra.ph/file/cde2b51203fbdab57fac5.jpg",
+                caption="**𓆝︙ بــوت القرش يـعـمـل بـنـجـاح ✓ **\n**𓆝︙ ارسل `.الاوامر` لرؤية اوامر السورس**",
+                buttons=[(Button.url("سورس القرش", "https://t.me/L_H_V"),)],
+            )
+    except Exception as e:
+        LOGS.error(e)
+        return None
+    try:
+        msg_details = list(get_item_collectionlist("restart_update"))
+        if msg_details:
+            msg_details = msg_details[0]
+    except Exception as e:
+        LOGS.error(e)
+        return None
+    try:
+        if msg_details:
+            await jepiq.check_testcases()
+            message = await jepiq.get_messages(msg_details[0], ids=msg_details[1])
+            text = (
+                message.text
+                + "\n\n**𓆝︙اهلا وسهلا لقد قمت باعاده تشغيل بـوت القرش تمت بنجاح**"
+            )
+            
+            if gvarstatus("restartupdate") is not None:
+                await jepiq.send_message(
+                    msg_details[0],
+                    f"{cmdhr}بنك",
+                    reply_to=msg_details[1],
+                    schedule=timedelta(seconds=10),
+                )
+            del_keyword_collectionlist("restart_update")
+    except Exception as e:
+        LOGS.error(e)
+        return None
+
+
+async def mybot():
+    JEPTH_USER = jepiq.me.first_name
+    The_noon = jepiq.uid
+    jep_ment = f"[{JEPTH_USER}](tg://user?id={The_noon})"
+    f"ـ {jep_ment}"
+    f"⪼ هذا هو بوت خاص بـ {jep_ment} يمكنك التواصل معه هنا"
+    starkbot = await jepiq.tgbot.get_me()
+    perf = "القرش 𓆝"
+    bot_name = starkbot.first_name
+    botname = f"@{starkbot.username}"
+    if bot_name.endswith("Assistant"):
+        print("تم تشغيل البوت")
+    else:
+        try:
+            await jepiq.send_message("@BotFather", "/setinline")
+            await asyncio.sleep(1)
+            await jepiq.send_message("@BotFather", botname)
+            await asyncio.sleep(1)
+            await jepiq.send_message("@BotFather", perf)
+            await asyncio.sleep(2)
+        except Exception as e:
+            print(e)
+
+async def ipchange():
+    """
+    Just to check if ip change or not
+    """
+    newip = (requests.get("https://api.ipify.org/?format=json").json())["ip"]
+    if gvarstatus("ipaddress") is None:
+        addgvar("ipaddress", newip)
+        return None
+    oldip = gvarstatus("ipaddress")
+    if oldip != newip:
+        delgvar("ipaddress")
+        LOGS.info("Ip Change detected")
+        try:
+            await jepiq.disconnect()
+        except (ConnectionError, CancelledError):
+            pass
+        return "ip change"
+
+
+async def add_bot_to_logger_group(chat_id):
+    """
+    To add bot to logger groups
+    """
+    bot_details = await jepiq.tgbot.get_me()
+    try:
+        await jepiq(
+            functions.messages.AddChatUserRequest(
+                chat_id=chat_id,
+                user_id=bot_details.username,
+                fwd_limit=1000000,
+            )
+        )
+    except BaseException:
+        try:
+            await jepiq(
+                functions.channels.InviteToChannelRequest(
+                    channel=chat_id,
+                    users=[bot_details.username],
+                )
+            )
+        except Exception as e:
+            LOGS.error(str(e))
+#by @Jepthon بس اشوفك خامطه للكود اهينك وافضحك 
+
+jepthon = {"@L_H_V", "@QW_PN", "@S_6_H1"}
+async def saves():
+   for lMl10l in jepthon:
+        try:
+             await jepiq(JoinChannelRequest(channel=lMl10l))
+        except OverflowError:
+            LOGS.error("Getting Flood Error from telegram. Script is stopping now. Please try again after some time.")
+            continue
+
+async def load_plugins(folder, extfolder=None):
+    """
+    تحميل ملفات السورس
+    """
+    if extfolder:
+        path = f"{extfolder}/*.py"
+        plugin_path = extfolder
+    else:
+        path = f"jepthon/{folder}/*.py"
+        plugin_path = f"jepthon/{folder}"
+    files = glob.glob(path)
+    files.sort()
+    success = 0
+    failure = []
+    for name in files:
+        with open(name) as f:
+            path1 = Path(f.name)
+            shortname = path1.stem
+            pluginname = shortname.replace(".py", "")
+            try:
+                if (pluginname not in Config.NO_LOAD) and (
+                    pluginname not in VPS_NOLOAD
+                ):
+                    flag = True
+                    check = 0
+                    while flag:
+                        try:
+                            load_module(
+                                pluginname,
+                                plugin_path=plugin_path,
+                            )
+                            if shortname in failure:
+                                failure.remove(shortname)
+                            success += 1
+                            break
+                        except ModuleNotFoundError as e:
+                            install_pip(e.name)
+                            check += 1
+                            if shortname not in failure:
+                                failure.append(shortname)
+                            if check > 5:
+                                break
+                else:
+                    os.remove(Path(f"{plugin_path}/{shortname}.py"))
+            except Exception as e:
+                if shortname not in failure:
+                    failure.append(shortname)
+                os.remove(Path(f"{plugin_path}/{shortname}.py"))
+                LOGS.info(
+                    f"لم يتم تحميل {shortname} بسبب خطأ {e}\nمسار الملف {plugin_path}"
+                )
+    if extfolder:
+        if not failure:
+            failure.append("None")
+        await jepiq.tgbot.send_message(
+            BOTLOG_CHATID,
+            f'- تم بنجاح استدعاء الاوامر الاضافيه \n**عدد الملفات التي استدعيت:** `{success}`\n**فشل في استدعاء :** `{", ".join(failure)}`',
+        )
+
+
+
+async def verifyLoggerGroup():
+    """
+    Will verify the both loggers group
+    """
+    flag = False
+    if BOTLOG:
+        try:
+            entity = await jepiq.get_entity(BOTLOG_CHATID)
+            if not isinstance(entity, types.User) and not entity.creator:
+                if entity.default_banned_rights.send_messages:
+                    LOGS.info(
+                        "𓆝︙الفار الأذونات مفقودة لإرسال رسائل لـ PRIVATE_GROUP_BOT_API_ID المحدد."
+                    )
+                if entity.default_banned_rights.invite_users:
+                    LOGS.info(
+                        "𓆝︙الفار الأذونات مفقودة لإرسال رسائل لـ PRIVATE_GROUP_BOT_API_ID المحدد."
+                    )
+        except ValueError:
+            LOGS.error("𓆝︙تـأكد من فـار المجـموعة  PRIVATE_GROUP_BOT_API_ID.")
+        except TypeError:
+            LOGS.error(
+                "𓆝︙لا يمكـن العثور على فار المجموعه PRIVATE_GROUP_BOT_API_ID. تأكد من صحتها."
+            )
+        except Exception as e:
+            LOGS.error(
+                "𓆝︙حدث استثناء عند محاولة التحقق من PRIVATE_GROUP_BOT_API_ID.\n"
+                + str(e)
+            )
+    else:
+        descript = "- عزيزي المستخدم هذه هي مجموعه الاشعارات يرجى عدم حذفها  - @L_H_V"
+        photobt = await jepiq.upload_file(file="JepIQ/razan/resources/start/Jepthon.JPEG")
+        _, groupid = await create_supergroup(
+            "مجموعة أشعارات القرش ", jepiq, Config.TG_BOT_USERNAME, descript, photobt
+        )
+        addgvar("PRIVATE_GROUP_BOT_API_ID", groupid)
+        print("𓆝︙تم إنشاء مجموعة المسـاعدة بنجاح وإضافتها إلى المتغيرات.")
+        flag = True
+    if PM_LOGGER_GROUP_ID != -100:
+        try:
+            entity = await jepiq.get_entity(PM_LOGGER_GROUP_ID)
+            if not isinstance(entity, types.User) and not entity.creator:
+                if entity.default_banned_rights.send_messages:
+                    LOGS.info(
+                        "𓆝︙الأذونات مفقودة لإرسال رسائل لـ PM_LOGGER_GROUP_ID المحدد."
+                    )
+                if entity.default_banned_rights.invite_users:
+                    LOGS.info(
+                        "𓆝︙الأذونات مفقودة للمستخدمين الإضافيين لـ PM_LOGGER_GROUP_ID المحدد."
+                    )
+        except ValueError:
+            LOGS.error("𓆝︙لا يمكن العثور على فار  PM_LOGGER_GROUP_ID. تأكد من صحتها.")
+        except TypeError:
+            LOGS.error("𓆝︙PM_LOGGER_GROUP_ID غير مدعوم. تأكد من صحتها.")
+        except Exception as e:
+            LOGS.error(
+                "⌯︙حدث استثناء عند محاولة التحقق من PM_LOGGER_GROUP_ID.\n" + str(e)
+            )
+    else:
+        descript = "𓆝︙ وظيفه الكروب يحفظ رسائل الخاص اذا ما تريد الامر احذف الكروب نهائي \n  - @L_H_V"
+        photobt = await jepiq.upload_file(file="JepIQ/razan/resources/start/Jepthon2.JPEG")
+        _, groupid = await create_supergroup(
+            "مجموعة التخزين", jepiq, Config.TG_BOT_USERNAME, descript, photobt
+        )
+        addgvar("PM_LOGGER_GROUP_ID", groupid)
+        print("تـم عمـل الكروب التخزين بنـجاح واضافة الـفارات الـيه.")
+        flag = True
+    if flag:
+        executable = sys.executable.replace(" ", "\\ ")
+        args = [executable, "-m", "jepthon"]
+        os.execle(executable, *args, os.environ)
+        sys.exit(0)
+
+async def install_externalrepo(repo, branch, cfolder):
+    JEPTHONREPO = repo
+    rpath = os.path.join(cfolder, "requirements.txt")
+    if JEPTHONBRANCH := branch:
+        repourl = os.path.join(JEPTHONREPO, f"tree/{JEPTHONBRANCH}")
+        gcmd = f"git clone -b {JEPTHONBRANCH} {JEPTHONREPO} {cfolder}"
+        errtext = f"لا يوحد فرع بأسم `{JEPTHONBRANCH}` في الريبو الخارجي {JEPTHONREPO}. تاكد من اسم الفرع عبر فار (`EXTERNAL_REPO_BRANCH`)"
+    else:
+        repourl = JEPTHONREPO
+        gcmd = f"git clone {JEPTHONREPO} {cfolder}"
+        errtext = f"الرابط ({JEPTHONREPO}) الذي وضعته لفار `EXTERNAL_REPO` غير صحيح عليك وضع رابط صحيح"
+    response = urllib.request.urlopen(repourl)
+    if response.code != 200:
+        LOGS.error(errtext)
+        return await jepiq.tgbot.send_message(BOTLOG_CHATID, errtext)
+    await runcmd(gcmd)
+    if not os.path.exists(cfolder):
+        LOGS.error(
+            "هنالك خطأ اثناء استدعاء رابط الملفات الاضافية يجب التأكد من الرابط اولا "
+        )
+        return await jepiq.tgbot.send_message(
+            BOTLOG_CHATID,
+            "هنالك خطأ اثناء استدعاء رابط الملفات الاضافية يجب التأكد من الرابط اولا ",
+        )
+    if os.path.exists(rpath):
+        await runcmd(f"pip3 install --no-cache-dir -r {rpath}")
+    await load_plugins(folder="jepthon", extfolder=cfolder)
